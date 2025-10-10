7b7637416d496ae9264493b6de150417e6afadb1
ini <vinothini.senthilkumarc@gmail.com>
2025-10-10 14:18:38 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -4,7 +4,9 @@ const UserSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  interests: [String]
+  interests: [String],
+  profilePicture: { type: String, default: "" } // <-- New field
 });
+
 
 export default mongoose.model("User", UserSchema);
