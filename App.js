1da4e724a54556632b7657f906664f27cb74c33f
ini <vinothini.senthilkumarc@gmail.com>
2025-10-23 11:14:33 +0530

    Update App.js

--- App.js
+++ App.js
@@ -6,6 +6,8 @@ import Profile from "./pages/Profile"; // other users
 import Navbar from "./components/Navbar";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
+import Rooms from "./pages/Rooms";
+
 
 function App() {
   return (
@@ -16,7 +18,8 @@ function App() {
         <Route path="/profile" element={<ProfilePage />} /> {/* current user */}
         <Route path="/profile/:id" element={<Profile />} /> {/* other users */}
         <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
+        <Route path="/signup" element={<Signup />} />           
+        <Route path="/rooms" element={<Rooms />} />
       </Routes>
     </Router>
   );
