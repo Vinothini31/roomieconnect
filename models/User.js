c1dc20cca24f1c2d954964989d99eb79904f0cff
ini <vinothini.senthilkumarc@gmail.com>
2025-10-23 10:54:42 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -5,7 +5,8 @@ const UserSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   interests: [String],
-  profilePicture: { type: String, default: "" } // <-- New field
+  profilePicture: { type: String, default: "" } ,// <-- New field
+  roomNumber: { type: String, default: null }
 });
 
 
