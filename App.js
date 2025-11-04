1f97b098a397b15de422ce202dfaa8c770a1b8fc
ini <vinothini.senthilkumarc@gmail.com>
2025-11-04 21:02:16 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 // frontend/src/App.js
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate, useLocation } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import ProfilePage from "./pages/ProfilePage";
 import Profile from "./pages/Profile"; // other users
@@ -8,21 +8,34 @@ import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Rooms from "./pages/Rooms";
 
+function AppContent() {
+  const location = useLocation();
 
-function App() {
+  // ✅ Hide Navbar on login and signup pages
+  const hideNavbar = location.pathname === "/login" || location.pathname === "/signup";
+
   return (
-    <Router>
-      <Navbar /> {/* ✅ Navbar shown on all pages */}
+    <>
+      {!hideNavbar && <Navbar />}
       <Routes>
+        {/* 👇 Default route now redirects to /login */}
+        <Route path="/" element={<Navigate to="/login" replace />} />
+
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/profile" element={<ProfilePage />} /> {/* current user */}
         <Route path="/profile/:id" element={<Profile />} /> {/* other users */}
         <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />           
+        <Route path="/signup" element={<Signup />} />
         <Route path="/rooms" element={<Rooms />} />
       </Routes>
-    </Router>
+    </>
   );
 }
 
-export default App;
+export default function App() {
+  return (
+    <Router>
+      <AppContent />
+    </Router>
+  );
+}
